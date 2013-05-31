fa90761dc3aec1212dd9a0d61aa4b21df96f42bb
qinchuan <qinchuan@qinchuan-Lenovo.(none)>
2013-05-31 10:15:09 +0800

    有向图的强连通分量+缩点

--- Graph/poj1236.cpp
+++ Graph/poj1236.cpp
@@ -1,3 +1,10 @@
+
+/***********************************************
+学校之间有单向的网络，每个学校得到一套软件后，可以通过单向网络向周边的学校传输.
+1:初始至少需要向多少个学校发放软件，使得网络内所有的学校最终都能得到软件。
+2:至少需要添加几条传输线路，使任意向一个学校发放软件后，经过传送，所有学校都能得到软件。
+************************************************/
+
 #include <stdio.h>
 #include <string.h>
 #include <stack>
@@ -13,7 +20,7 @@ struct node{
 int head[maxn], sccno[maxn], in[maxn], out[maxn];
 int low[maxn], dfn[maxn];
 bool iscut[maxn];
-int n, m, cnt, scc_cnt, dfs_clock;
+int n, cnt, scc_cnt, dfs_clock;
 std::stack<int>st;
 
 void init(){
